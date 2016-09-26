ee11399381c450ba99ddde08733e1e00355fd834
krychynska <krychynska2@gmail.com>
2016-09-27 01:11:15 +0300

    server added and works for now

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 import express from 'express';
-const  app = express();
+import bodyParser from 'body-parser';
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+import * as db from './data_base/data.base.js';
+import { serverPort } from '../config/config.json';
+
+db.setupConnection();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/notes', (req, res) => {
+    db.notesList().then(data => res.send(data));
 });
 
-const  server = app.listen(8080, () => {
-        console.log("server is up and running");
-    });
+app.post('/notes', (req, res) => {
+    db.createNote(req.body).then(data => res.send(data));
+});
+app.post('/notes/:id', (req, res) => {
+    db.deleteNote(req.params.id).then(data => res.send(data));
+});
+
+const server = app.listen(8080, () => {
+    console.log("server is up and running");
+});
